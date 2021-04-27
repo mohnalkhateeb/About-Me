1e1aebf751145ca4982da384648e9e9e02105de0
mohnalkhateeb <mhmmd.alkateeb@gmail.com>
2021-04-27 19:04:37 +0300

    lab03-1

--- js/app.js
+++ js/app.js
@@ -134,20 +134,56 @@ switch(food)
 alert('we wish nice mood to you '+ username)
 let rightnum = 13
 let num1 = parseInt(prompt('please, enter number between 0 and 20'))
+let corrnum=0;
+let count1=5;
 for (let i=1;i<=3;i++)
 {
     if (num1===rightnum){
         alert('Congratulations, you got the right answer')
+        console.log('Congratulations, you got the right answer')
+        count1= count1+i;
+        corrnum=num1;
         break
     }
     else if(num1 >= (rightnum-3) && num1 <= (rightnum+3)){
         alert('You are close to the correct answer, try again')
+        console.log('You are close to the correct answer, try again')
         num1 = parseInt(prompt('please, enter number between 0 and 20'))
     }
     else
     {
         alert('You are far from the correct answer, try again')
+        console.log('You are close to the correct answer, try again')
         num1 = parseInt(prompt('please, enter number between 0 and 20'))
     }
 }
-alert('the right answer is '+rightnum)
+if ( corrnum === 0){alert('sorry, the right answer is '+rightnum)}
+
+alert('no problem, now we will give you a chance and there will be four correct answer and you may hit one of them')
+let rightnums = [11,17,23,36,8,31]
+let count2 =0 
+let flag = 0
+let num2 
+for (let k=1;k<=6;k++){
+    if (flag === 1){break}
+    else{num2=parseInt(prompt('please, enter number between 0 and 40'))}
+
+ 
+for (let j = 0; j < rightnums.length; j++) {
+    if (num2 === rightnums[j]) {
+        console.log('yes ,you have correct one ')
+        alert('Congratulations, you got the right answer')
+        count1=count1+1
+        flag=1
+        break
+
+    }
+    else{count2 = j}
+}
+
+}
+if (count2 >=(rightnums.length-1)) {
+    console.log('sorry it is not one of them')
+    alert(alert('sorry, the right answer is one of them '+rightnums))
+}
+alert('your score is ' + count1 +'"\"7')
