b63fce3226dfeda6637991c95fd10e334b3af03f
mohnalkhateeb <mhmmd.alkateeb@gmail.com>
2021-04-29 17:07:54 +0300

    finish1

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 'use strict'
+/*let oddeven = function(funarray)
+{
+    let odev= [0,0]
+    for (let c1 = 0; c1 < funarray.length; c1++)
+    {
+        if (funarray[c1]%2 == 0)
+        odev[0]= odev[0]+1
+        else odev[1]= odev[1]+1
+    }
+    return odev
+}
+let Array1= [100,20,33,45,44,37,10,2,3,5,15,12,31,32,88,94,62,63,77,66,27,1,7,8,9]
+let odevcount = oddeven(Array1)
+console.log('number of even numbers is ' + odevcount[0])
+console.log('number of odd numbers is ' + odevcount[1]) */
 let username = prompt('what is your name?')
 alert('Hello '+ username);
 
@@ -19,10 +34,43 @@ switch(mood)
     
 }
 
+let pics = ['sea', 'forest', 'derest' , 'Mountain' , 'snow' , 'food']
+let imag = ['<img src="img/sea.jpg" alt="sea">',
+'<img src="img/forest.jpg" alt="forest">',
+'<img src="img/desert.jpg" alt="desert">',
+'<img src="img/mountain.jpg" alt="Mountain">',
+'<img src="img/snow.jpg" alt="snow">',
+'<img src="img/food.jpg" alt="food">']
 
-function Q1();
+function Quastion(word,image)
+{
+    for (let c1 = 0; c1 < word.length; c1++)
+    {
+     let pic = prompt(`do you like ${word[c1]} ? yes or no`).toLowerCase()
+    switch(pic)
+    {
+        case "yes":
+        alert(`this is good. enjoy with ${word[c1]}`)
+        document.write(image[c1])
+        break;
+        case "no":
+            alert(`try change your mood with ${word[c1]}`)
+            document.write(image[c1])
+        break;
+        default :
+        alert('please make sure your answer yes or no')
+        document.write(image[c1])
+        pic =prompt(`do you like ${word[c1]} ? yes or no`).toLowerCase()
+        break;
+    
+        
+    }   
 
-let sea = prompt('do you like sea ? yes or no').toLowerCase()
+    }
+    
+}
+Quastion(pics,imag)
+/*let sea = prompt('do you like sea ? yes or no').toLowerCase()
 switch(sea)
 {
     case "yes":
@@ -40,12 +88,12 @@ switch(sea)
     break;
 
     
-}
-Q1();
+}*/
+// Q1();
 
-function Q2();
+// function Q2();
 
-let forest = prompt('do you like forest ? yes or no').toLowerCase()
+/*let forest = prompt('do you like forest ? yes or no').toLowerCase()
 switch(forest)
 {
     case "yes":
@@ -65,10 +113,10 @@ switch(forest)
     
 }
 
-Q2();
+// Q2();
 
 
-function Q3();
+// function Q3();
 
 
 let desert = prompt('do you like dessert ? yes or no').toLowerCase()
@@ -91,10 +139,10 @@ switch(desert)
     
 }
 
-Q3();
+// Q3();
 
 
-function Q4();
+// function Q4();
 
 
 let Mountain = prompt('do you like Mountain ? yes or no').toLowerCase()
@@ -117,9 +165,9 @@ switch(Mountain)
     
 }
 
-Q4();
+// Q4();
 
-function Q5();
+// function Q5();
 
 
 let snow = prompt('do you like snow ? yes or no').toLowerCase()
@@ -140,13 +188,13 @@ switch(snow)
     break;
 
     
-}
+}*/
 
-Q5();
+// Q5();
 
-function Q6();
+// function Q6();
 
-let food = prompt('are you hungry ? yes or no').toLowerCase()
+/*let food = prompt('are you hungry ? yes or no').toLowerCase()
 switch(food)
 {
     case "yes":
@@ -164,62 +212,73 @@ switch(food)
     break;
 
     
-}
+}*/
 
-Q6()
+// Q6()
 
 
-function Q7();
+// function Q7();
 
 alert('we wish nice mood to you '+ username)
 let rightnum = 13
 let num1 = parseInt(prompt('please, enter number between 0 and 20'))
-let corrnum=0;
+
 let count1=5;
-for (let i=1;i<=3;i++)
+function Quastion6(nm1,nm2)
 {
-    if (num1===rightnum){
+    let corrnum=0;
+   for (let i=1;i<=3;i++)
+{
+    if (nm1===nm2){
         alert('Congratulations, you got the right answer')
         console.log('Congratulations, you got the right answer')
-        count1= count1+i;
-        corrnum=num1;
+        count1= count1+1;
+        corrnum=nm1;
         break
     }
-    else if(num1 >= (rightnum-3) && num1 <= (rightnum+3)){
+    else if(nm1 >= (nm2-3) && nm1 <= (nm2+3)){
         alert('You are close to the correct answer, try again')
         console.log('You are close to the correct answer, try again')
-        num1 = parseInt(prompt('please, enter number between 0 and 20'))
+        nm1 = parseInt(prompt('please, enter number between 0 and 20'))
     }
     else
     {
         alert('You are far from the correct answer, try again')
         console.log('You are close to the correct answer, try again')
-        num1 = parseInt(prompt('please, enter number between 0 and 20'))
+        nm1 = parseInt(prompt('please, enter number between 0 and 20'))
     }
 }
-if ( corrnum === 0){alert('sorry, the right answer is '+rightnum)}
+if ( corrnum === 0){alert('sorry, the right answer is '+nm2)} 
+}
+Quastion6(num1,rightnum);
 
 alert('no problem, now we will give you a chance and there will be four correct answer and you may hit one of them')
+
 let rightnums = [11,17,23,36,8,31]
 let count2 =0 
 let flag = 0
+function Quastion7(arr1,testnum)
+{
+    for (let j = 0; j < arr1.length; j++) {
+        if (testnum === arr1[j]) {
+            console.log('yes ,you have correct one ')
+            alert('Congratulations, you got the right answer')
+            
+            flag=1
+            break
+    
+        }
+        else{count2 = j}
+    }
+}
 let num2 
 for (let k=1;k<=6;k++){
-    if (flag === 1){break}
+    if (flag === 1){ count1=count1+1
+        break}
     else{num2=parseInt(prompt('please, enter number between 0 and 40'))}
 
  
-for (let j = 0; j < rightnums.length; j++) {
-    if (num2 === rightnums[j]) {
-        console.log('yes ,you have correct one ')
-        alert('Congratulations, you got the right answer')
-        count1=count1+1
-        flag=1
-        break
-
-    }
-    else{count2 = j}
-}
+    Quastion7(rightnums,num2)
 
 }
 if (count2 >=(rightnums.length-1)) {
@@ -228,4 +287,4 @@ if (count2 >=(rightnums.length-1)) {
 }
 alert('your score is ' + count1 +'"\"7')
 
-Q7(); 
+// Q7(); 
